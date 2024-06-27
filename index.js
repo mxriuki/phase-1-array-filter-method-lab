7a5294e54962fc4330113191feb7cddb806a12b2
mxriuki <mwendav37@gmail.com>
2024-06-27 09:41:57 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+function findMatching(drivers, name) {
+  let drivers_match = drivers.filter((element) => {
+    return element.charAt(0).toUpperCase() + element.slice(1) === name;
+  });
+  return drivers_match;
+}
+function fuzzyMatch(drivers, name) {
+  let drivers_match = drivers.filter((element) => {
+    return element.slice(0, 2) === name;
+  });
+  return drivers_match;
+}
+function matchName(drivers, name) {
+  let drivers_match = drivers.filter((element) => {
+    return element.name === name;
+  });
+  return drivers_match;
+}
+findMatching(drivers, "Sa");
