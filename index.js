f2ae2ad3bc99618540455d521926e03a074219cc
mxriuki <mwendav37@gmail.com>
2024-07-10 17:29:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 // Code your solution here
-const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
-function findMatching(drivers, name) {
-  let drivers_match = drivers.filter((element) => {
-    return element.charAt(0).toUpperCase() + element.slice(1) === name;
-  });
-  return drivers_match;
+function superbowlWin(record) {
+  const win = record.find((game) => game.result === "W");
+  return win ? win.year : undefined;
 }
-function fuzzyMatch(drivers, name) {
-  let drivers_match = drivers.filter((element) => {
-    return element.slice(0, 2) === name;
-  });
-  return drivers_match;
-}
-function matchName(drivers, name) {
-  let drivers_match = drivers.filter((element) => {
-    return element.name === name;
-  });
-  return drivers_match;
-}
-findMatching(drivers, "Sa");
+superbowlWin(record);
